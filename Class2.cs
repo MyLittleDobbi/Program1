87e7c63339c4883a94ba21cfcaaeef119e994743
MyLittleDobbi <gameryura96@gmail.com>
2015-09-01 23:38:58 +0300

    d

---
+++ Class2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Program1
+{
+    class Class1
+    {
+        public int j;
+        public void F()
+        {
+            j++;
+          
+        }
+        public int a;
+        public int f()
+        {
+            //while (true) { }
+            return 3;
+        }
+    }
+    class Class2 :Class1
+    {
+        public int b=45;
+        public int B { set; get; }
+        public Class2()
+        {
+           // b =a;
+            i++;
+        }
+        public int dF()
+        {
+            b = B;
+            return b;
+        }
+        private static int i = 0;
+        new public int f( ) 
+        {
+            //while (true) { }
+            return 2;
+        }
+    }
+    public class A
+    {
+        public int pub { get; set;}
+        private int pri { get; set; }
+        protected int pro { get; set; }
+    }
+
+
+
+    public interface A1
+    {
+
+    }
+    public interface A3
+    {
+
+    }
+    public class A2
+    {
+
+
+    }
+    public class B: A2 , A1 
+    {
+
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1[] A= new Class1[2];
+            int[] M=new int[3];
+            
+            A[0] = new Class1();
+            A[0].a = 8;
+            
+            Console.WriteLine("{0}, {0}", "d");
+        }
+    }
+}
